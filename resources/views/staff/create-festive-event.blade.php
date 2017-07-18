bfa8e627f72b6b4e129566600db975177eeb3e5e
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-18 11:57:27 +0800

    modify create festive event 1

--- resources/views/staff/create-festive-event.blade.php
+++ resources/views/staff/create-festive-event.blade.php
@@ -1,1 +1,27 @@
-<h1>Here</h1>
+@extends('layouts.backend.app')
+
+@section('main-content')
+
+    <div class="page-container-fluid">
+
+        <div class="page-content-wrapper">
+
+          <div class="page-head">
+
+              <div class="container-fluid">
+
+                  <div class="page-title">
+
+                      <h1>Event  Calendar 庆典节目表</h1>
+
+                  </div><!-- end page-title -->
+
+              </div><!-- end container-fluid -->
+
+          </div><!-- end page-head -->
+
+        </div><!-- end page-content-wrapper -->
+
+    </div>!<!-- end page-container-fluid -->
+
+@stop
