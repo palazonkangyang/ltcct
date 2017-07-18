3dbd620e90e97574dfac9ea047521b45eda2e2f4
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-18 12:01:02 +0800

    modify create festive event 2

--- resources/views/staff/create-festive-event.blade.php
+++ resources/views/staff/create-festive-event.blade.php
@@ -20,6 +20,40 @@
 
           </div><!-- end page-head -->
 
+          <div class="page-content">
+
+              <div class="container-fluid">
+
+                  <ul class="page-breadcrumb breadcrumb">
+                      <li>
+                          <a href="/operator/index">Home</a>
+                          <i class="fa fa-circle"></i>
+                      </li>
+                      <li>
+                          <span>General Donation</span>
+                      </li>
+                  </ul>
+
+                  <div class="page-content-inner">
+
+                      <div class="inbox">
+
+                          <div class="row">
+
+                              <div class="col-md-12">
+
+                              </div><!-- end col-md-12 -->
+
+                          </div><!-- end row -->
+
+                      </div><!-- end inbox -->
+
+                  </div><!-- end page-content-inner -->
+
+              </div><!-- end container-fluid -->
+
+          </div><!-- end page-content -->
+
         </div><!-- end page-content-wrapper -->
 
     </div>!<!-- end page-container-fluid -->
