779e098b207b64ed1d9ae81ef5a423a5299c6e23
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-19 17:49:30 +0800

    change header css

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -35,6 +35,7 @@
 		<link href="{{ asset('/css/ootstrap-datetimepicker.min.css') }}" rel="stylesheet" type="text/css">
 
 		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css">
 
     <style type="text/css">
         tfoot input {
