cd94b0b0f41ac2bde23cf49313eb095bed29e248
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-19 14:13:32 +0800

    add Fund account navigation

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -93,7 +93,7 @@
                                     </li>
 
                                     <li aria-haspopup="true" class="menu-dropdown mega-menu-dropdown">
-                                        <a href="/staff/donation"> General Donation 乐捐
+                                        <a href="/staff/donation" class="nav-link  "> General Donation 乐捐
                                             <span class="arrow"></span>
                                         </a>
                                     </li>
@@ -103,6 +103,83 @@
                                             <span class="arrow"></span>
                                         </a>
                                     </li>
+
+                                    <li aria-haspopup="true" class="menu-dropdown mega-menu-dropdown  mega-menu-full">
+                                            <a href="javascript:;"> Fund Account
+                                                <span class="arrow"></span>
+                                            </a>
+                                            <ul class="dropdown-menu" style="min-width: ">
+                                                <li>
+                                                    <div class="mega-menu-content">
+                                                        <div class="row">
+                                                            <div class="col-md-3">
+                                                                <ul class="mega-menu-submenu">
+                                                                    <li>
+                                                                        <h3>Income & Expenditure</h3>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">Income</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">Expenditure</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">Receive</a>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-md-3">
+                                                                <ul class="mega-menu-submenu">
+                                                                    <li>
+                                                                        <h3>Setting</h3>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">Fiscal Year</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">Jobs</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">Cost Center</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">GL Account Group</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">GL Accounts</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">Chart All Accounts</a>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-md-3">
+                                                                <ul class="mega-menu-submenu">
+                                                                    <li>
+                                                                        <h3>Finance</h3>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="#">General Entry</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="components_bootstrap_touchspin.html">Income Statement Report</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="components_form_tools.html">Trial Balance Report</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="components_context_menu.html">Cashflow Statement Report</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="components_editors.html"> Markdown & WYSIWYG Editors </a>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </li>
 
                                 </ul><!-- end nav navbar-nav -->
                             </div><!-- end hor-menu -->
