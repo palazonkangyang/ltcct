98eca9504bdbab7209df58f2987ddc836c48c656
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-19 17:47:50 +0800

    change datatable for glaccountgroup

--- resources/views/account/glaccountgroup.blade.php
+++ resources/views/account/glaccountgroup.blade.php
@@ -249,8 +249,15 @@
 
 @section('custom-js')
 
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+
   <script type="text/javascript">
     $(function() {
+
+      $('#glaccountgroup-table').DataTable( {
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
+      });
 
       $('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
           localStorage.setItem('activeTab', $(e.target).attr('href'));
