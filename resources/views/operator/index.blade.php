fa665d20880d774c4d08c454320bd2c2cf182a3f
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-24 14:48:45 +0800

    modify focus devotee 69

--- resources/views/operator/index.blade.php
+++ resources/views/operator/index.blade.php
@@ -2110,7 +2110,31 @@
 						        dataType: 'json',
 						        success: function(response)
 						        {
-											console.log(response);
+											$("#special_remark").empty();
+											$("#opt_address").empty();
+											$("#opt_vehicle").empty();
+
+											$("#edit_devotee_id").val(response.devotee['devotee_id']);
+											$("#edit_chinese_name").val(response.devotee['chinese_name']);
+											$("#edit_english_name").val(response.devotee['english_name']);
+											$("#edit_contact").val(response.devotee['contact']);
+											$("#edit_guiyi_name").val(response.devotee['guiyi_name']);
+											$("#edit_address_houseno").val(response.devotee['address_houseno']);
+											$("#edit_address_unit1").val(response.devotee['address_unit1']);
+											$("#edit_address_unit2").val(response.devotee['address_unit2']);
+											$("#edit_address_street").val(response.devotee['address_street']);
+											$("#edit_address_building").val(response.devotee['address_building']);
+											$("#edit_address_postal").val(response.devotee['address_postal']);
+											$("#edit_address_translate").val(response.devotee['address_translate']);
+											$("#edit_oversea_addr_in_china").val(response.devotee['oversea_addr_in_chinese']);
+											$("#edit_nric").val(response.devotee['nric']);
+											$("#edit_deceased_year").val(response.devotee['deceased_year']);
+											$("#edit_dob").val(response.devotee['dob']);
+											$("#edit_marital_status").val(response.devotee['marital_status']);
+											$("#edit_dialect").val(response.devotee['dialect']);
+											$("#edit_nationality").val(response.devotee['nationality']);
+											$("#edit_familycode_id").val(response.devotee['familycode_id']);
+											$("#edit_member_id").val(response.devotee['member_id']);
 						        },
 
 						        error: function (response) {
