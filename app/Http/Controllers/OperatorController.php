27ec091c6100e7d717810df67ce31e20e60eb0ce
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-24 14:28:27 +0800

    modify focus devotee 63

--- app/Http/Controllers/OperatorController.php
+++ app/Http/Controllers/OperatorController.php
@@ -128,8 +128,8 @@ class OperatorController extends Controller
 	{
 	  $devotee_id = $_GET['devotee_id'];
 
-	  $devotee = Devotee::leftjoin('member', 'devotee.member_id', '=', 'member.member_id');
-							 ->leftjoin('familycode', 'devotee.familycode_id', '=', 'familycode.familycode_id');
+	  $devotee = Devotee::leftjoin('member', 'devotee.member_id', '=', 'member.member_id')
+							 ->leftjoin('familycode', 'devotee.familycode_id', '=', 'familycode.familycode_id')
 							 ->select('devotee.*', 'familycode.familycode', 'member.introduced_by1', 'member.introduced_by2', 'member.approved_date')
 							 ->where('devotee.devotee_id', $devotee_id)
 							 ->get();
