9d7531368a5f2c28016909f72251791026cd8b88
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-25 14:40:19 +0800

    change operator controller 6

--- app/Http/Controllers/OperatorController.php
+++ app/Http/Controllers/OperatorController.php
@@ -814,7 +814,7 @@ class OperatorController extends Controller
 
 			else
 			{
-				// $request->session()->flash('error', "Password did not match. Please Try Again");
+				$request->session()->flash("error", "Password did not match. Please Try Again");
 				return redirect()->back()->withInput();
 			}
 	}
