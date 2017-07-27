4761a0fac4eab35eb77e230ef5c92df32b707fba
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-27 13:54:22 +0800

    add job fields in festive events 12

--- resources/views/staff/create-festive-event.blade.php
+++ resources/views/staff/create-festive-event.blade.php
@@ -222,19 +222,23 @@
                   dataType: 'json',
                   success: function(response)
                   {
-                    alert(JSON.stringify(response));
+                    // alert(JSON.stringify(response));
+                    //
+                    // $.each(response.job, function(index, data) {
+                    //   $
+                    // });
 
-                    $.each(response.job, function(index, data) {
-                      $("#festive-event-table").append("<tr class='event-row'><td><i class='fa fa-minus-circle removeEventRow' aria-hidden='true'></i></td>" +
-                          "<td><select name='' class='form-control'><option value=''>Select Job</option><option value=''>" + data.job_reference_no  + "</option></select>" +
-                          "<td><input type='text' class='form-control' name='start_at[]' data-provide='datepicker' data-date-format='dd/mm/yyyy' value=''></td>" +
-                          "<td><input type='text' class='form-control' name='end_at[]' data-provide='datepicker' data-date-format='dd/mm/yyyy' value=''></td>" +
-                          "<td><input type='text' class='form-control' name='lunar_date[]' value=''></td>" +
-                          "<td><input type='text' class='form-control' name='event[]' value=''></td>" +
-                          "<td><input type='text' class='form-control timepicker timepicker-no-seconds' data-provide='timepicker' name='time[]' value=''></td>" +
-                          "<td><input type='text' class='form-control' name='shuwen_title[]' value=''></td>" +
-                          "<td><input type='hidden' name='display_hidden[]' value=''><input type='checkbox' name='display[]' value='' class='form-control'></td></tr>");
-                    });
+                    $('.joblist').append("<option>1</option><option>2</option>");
+
+                    $("#festive-event-table").append("<tr class='event-row'><td><i class='fa fa-minus-circle removeEventRow' aria-hidden='true'></i></td>" +
+                        "<td><select name='' class='form-control joblist'></select>" +
+                        "<td><input type='text' class='form-control' name='start_at[]' data-provide='datepicker' data-date-format='dd/mm/yyyy' value=''></td>" +
+                        "<td><input type='text' class='form-control' name='end_at[]' data-provide='datepicker' data-date-format='dd/mm/yyyy' value=''></td>" +
+                        "<td><input type='text' class='form-control' name='lunar_date[]' value=''></td>" +
+                        "<td><input type='text' class='form-control' name='event[]' value=''></td>" +
+                        "<td><input type='text' class='form-control timepicker timepicker-no-seconds' data-provide='timepicker' name='time[]' value=''></td>" +
+                        "<td><input type='text' class='form-control' name='shuwen_title[]' value=''></td>" +
+                        "<td><input type='hidden' name='display_hidden[]' value=''><input type='checkbox' name='display[]' value='' class='form-control'></td></tr>");
                   },
 
                   error: function (response) {
