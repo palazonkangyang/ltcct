2ccc21e8115f4f43a4fbb84d418d2f56fafa2dd2
oakkamyopalazon <oakka.myo@palazon.com>
2017-07-27 16:04:59 +0800

    change receipt layout 12

--- resources/views/staff/receipt.blade.php
+++ resources/views/staff/receipt.blade.php
@@ -29,25 +29,70 @@
 			<article>
 				<div id="leftcontent">
 
-					<div class="label-wrapper">
-						<div class="label-left">Receipt Date :</div><!-- end label-left -->
-						<div class="label-right">{{ \Carbon\Carbon::parse($receipt[0]->trans_date)->format("d/m/Y") }}</div><!-- end label-right -->
-					</div><!-- end label-wrapper -->
+					<div class="receipt-info">
+						<div class="label-wrapper">
+							<div class="label-left">Receipt Date :</div><!-- end label-left -->
+							<div class="label-right">{{ \Carbon\Carbon::parse($receipt[0]->trans_date)->format("d/m/Y") }}</div><!-- end label-right -->
+						</div><!-- end label-wrapper -->
 
-					<div class="label-wrapper">
-						<div class="label-left">Paid By :</div><!-- end label-left -->
-						<div class="label-right">{{ $receipt[0]->chinese_name }} (D - {{ $receipt[0]->devotee_id }})</div><!-- end label-right -->
-					</div><!-- end label-wrapper -->
+						<div class="label-wrapper">
+							<div class="label-left">Paid By :</div><!-- end label-left -->
+							<div class="label-right">{{ $receipt[0]->chinese_name }} (D - {{ $receipt[0]->devotee_id }})</div><!-- end label-right -->
+						</div><!-- end label-wrapper -->
 
-					<div class="label-wrapper">
-						<div class="label-left">Description :</div><!-- end label-left -->
-						<div class="label-right">{{ $receipt[0]->description }}</div><!-- end label-right -->
-					</div><!-- end label-wrapper -->
+						<div class="label-wrapper">
+							<div class="label-left">Description :</div><!-- end label-left -->
+							<div class="label-right">{{ $receipt[0]->description }}</div><!-- end label-right -->
+						</div><!-- end label-wrapper -->
 
-					<div class="label-wrapper">
-						<div class="label-left">Donation for next Event :</div><!-- end label-left -->
-						<div class="label-right"></div><!-- end label-right -->	
-					</div><!-- end label-wrapper -->
+						<div class="label-wrapper">
+							<div class="label-left">Donation for next Event :</div><!-- end label-left -->
+							<div class="label-right"></div><!-- end label-right -->
+						</div><!-- end label-wrapper -->
+					</div>
+
+					<div class="receipt-list">
+
+						<table class="receipt-table">
+							<thead>
+								<tr>
+									<th>S/No</th>
+									<th>Chinese Name</th>
+									<th>Devotee</th>
+									<th>Block</th>
+									<th>Address</th>
+									<th>Unit</th>
+									<th>HJ/ GR</th>
+									<th>Receipt</th>
+									<th>Amount</th>
+								</tr>
+							</thead>
+
+							<tbody>
+
+								@php $count = 1; $sum= 0; @endphp
+
+								@foreach($donation_devotees as $donation_devotee)
+
+								<tr>
+									<td>{{ $count }}</td>
+									<td>{{ $donation_devotee->chinese_name }}</td>
+									<td>{{ $donation_devotee->devotee_id }}</td>
+									<td>{{ $donation_devotee->address_houseno }}</td>
+									<td>{{ $donation_devotee->address_street }}</td>
+									<td>{{ $donation_devotee->address_unit1 }} {{ $donation_devotee->address_unit2 }}</td>
+									<td>{{ $donation_devotee->hjgr }}</td>
+									<td>{{ $receipt[0]->xy_receipt }}</td>
+									<td>S$ {{ $donation_devotee->amount }}</td>
+								</tr>
+
+								@php $count++;  $sum += $donation_devotee->amount; @endphp
+
+								@endforeach
+							</tbody>
+						</table>
+
+					</div>
 
 				</div><!-- end leftcontent -->
 
