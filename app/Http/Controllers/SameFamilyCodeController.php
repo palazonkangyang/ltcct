ff4aa08a9e3822a9aef6ab71c3e264bdbb02d17d
allenwongpalazon <allen.wong@palazon.com>
2017-11-23 14:48:57 +0800

    created controllers and models for new table

---
+++ app/Http/Controllers/SameFamilyCodeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SameFamilyCodeController extends Controller
+{
+    //
+
+    public static function updateSameFamilyCode($mod_id){
+
+
+
+
+
+
+
+    }
+}
